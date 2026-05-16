999d4222cf8c4b6e1fd094916dfeb4bd642a74e1
cowhood <loanswipe@gmail.com>
2026-05-16 10:31:45 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next'
+import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'www.torquemade.com' },
@@ -10,4 +12,5 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+const withMDX = createMDX({})
+export default withMDX(nextConfig)
