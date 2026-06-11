f79750f6cf1d085c2097756bf8f2fcc69e19c335
loanrmb <loanswipe@gmail.com>
2026-06-11 14:10:45 +0200

    Update projects.ts

--- lib/projects.ts
+++ lib/projects.ts
@@ -383,7 +383,7 @@ export const projects: Project[] = [
     imageHero: '/images/sbbt.png',
     imageChallenge: '/images/sbbt.png',
     imageApproach: '/images/sbbt-challenge.png',
-    url: 'https://spicybeautybyt.fr',
+    url: 'https://spicybeautybyt.com',
     featured: false,
     caseStudy: true,
     challengeTitle: {
